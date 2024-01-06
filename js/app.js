0aa612956b714287607d6af961913d9be6be0c36
Valdeison Lima <valdeilsonbdl56@gmail.com>
2024-01-06 18:28:17 -0300

    Adicionando as demais possibilidade de escolha dos ingressos

--- js/app.js
+++ js/app.js
@@ -4,8 +4,12 @@ function comprar(){
 
     if(tipo.value == 'pista'){
         comprarPista(qtd);
+    } else if(tipo.value == 'superior'){
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd)
     }
-}     
+}
  
   function comprarPista(qtd){
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
@@ -14,7 +18,28 @@ function comprar(){
     }else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
-        alert('Compra realizada com sucesso!')
+        alert('Compra realizada com sucesso!');
     }
   }
     
+  function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(qtd > qtdSuperior){
+        alert('Quantidade indisponivel para tipo Superior');
+    }else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(qtd > qtdInferior){
+        alert('Quantidade indisponivel para tipo inferior');
+    }else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
